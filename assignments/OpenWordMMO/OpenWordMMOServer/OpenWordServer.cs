b33d34a22a7e5dc4c9340f844fe49fc2d7eb55ad
JonathanJonsson <jonsson.h.jonathan@gmail.com>
2022-04-28 20:13:29 +0200

    Added new UDP Client project

---
+++ assignments/OpenWordMMO/OpenWordMMOServer/OpenWordServer.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace OpenWordMMO;
+
+public class OpenWordServer
+{
+    static void Main()
+    {
+        var serverEndPoint = new IPEndPoint(IPAddress.Loopback, 1313);
+        var server = new UdpClient(serverEndPoint);
+
+        while (true)
+        {
+            IPEndPoint clientEndPoint = default;
+            var response = server.Receive(ref clientEndPoint);
+            Console.WriteLine($"Packets recived from: {clientEndPoint} saying: {Encoding.ASCII.GetString(response)}");
+        }
+    }
+}
