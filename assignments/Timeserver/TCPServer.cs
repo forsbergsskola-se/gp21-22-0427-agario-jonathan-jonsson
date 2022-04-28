7d06b9796e2fe647a5f7d4cb88262cadae908c38
JonathanJonsson <jonsson.h.jonathan@gmail.com>
2022-04-28 09:24:54 +0200

    Added so that we close stream as well

--- assignments/Timeserver/TCPServer.cs
+++ assignments/Timeserver/TCPServer.cs
@@ -2,7 +2,7 @@
 using System.Net.Sockets;
 using System.Text;
 
-var endPoint = new IPEndPoint(IPAddress.Loopback, 1441);
+var endPoint = new IPEndPoint(IPAddress.Loopback, 1111);
 
 var TCPListener = new TcpListener(endPoint);
 TCPListener.Start();
@@ -12,10 +12,14 @@ while (true)
     var TCPClient = TCPListener.AcceptTcpClient();
     var buffer = new byte[100];
     var responseBuffer = Encoding.ASCII.GetBytes("Type anything to get Date and time!");
+    
     TCPClient.GetStream().Write(responseBuffer, 0, responseBuffer.Length);
     TCPClient.GetStream().Read(buffer, 0, 100);
     Console.WriteLine("Client Input: " + Encoding.ASCII.GetString(buffer));
+    
     responseBuffer = Encoding.ASCII.GetBytes(DateTime.Now.ToString());
     TCPClient.GetStream().Write(responseBuffer, 0, responseBuffer.Length);
+    
+    TCPClient.GetStream().Close();
     TCPClient.Close();
-}o
+}
