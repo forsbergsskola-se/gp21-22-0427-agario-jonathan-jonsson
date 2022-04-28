3f54cdd9a0c32cf4e1a4552a0b0079c23b196e0f
JonathanJonsson <jonsson.h.jonathan@gmail.com>
2022-04-28 20:20:13 +0200

    Implemented basic UDP client

---
+++ assignments/OpenWordMMO/OpenWordMMOClient/OpenWordClient.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace OpenWordMMOClient;
+
+public class OpenWordClient
+{
+    static void Main()
+    {
+        var serverEndPoint = new IPEndPoint(IPAddress.Loopback, 1313);
+        var clientEndPoint = new IPEndPoint(IPAddress.Loopback, 1314);
+
+        var client = new UdpClient(clientEndPoint);
+        var message = Encoding.ASCII.GetBytes("Hello server");
+
+        client.Send(message, message.Length, serverEndPoint);
+    }
+}
