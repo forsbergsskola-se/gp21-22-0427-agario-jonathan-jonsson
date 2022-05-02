435179ac7f898734ae189cde9077f55fcf36967d
JonathanJonsson <jonsson.h.jonathan@gmail.com>
2022-05-02 10:12:37 +0200

    Fixed issue - now server recive mesages correctly
    Still not sending back though yet

--- assignments/Agario/Assets/UDPClient.cs
+++ assignments/Agario/Assets/UDPClient.cs
@@ -18,18 +18,13 @@ public class UDPClient : MonoBehaviour
 
     private UdpClient client;
 
-    private void Start()
-    {
-        Console.WriteLine("sss");
-    }
-
     public void SendChatMsg()
     {
-            client = new UdpClient(clientEndpoint);
             Debug.Log("Please enter a word, less than 20 characters. No whitespaces allowed");
+            client = new UdpClient(clientEndpoint);
             // var stringInput = Console.ReadLine();
             var stringInput = inputField.text;
-            if (stringInput == null) return;
+            if (string.IsNullOrEmpty(stringInput)) return;
             var message = Encoding.ASCII.GetBytes(stringInput);
             client.Send(message, message.Length, serverEndpoint);
             client.Close();
