38f83e27f1f71bab12beea88ee1804908a2c47a8
JonathanJonsson <jonsson.h.jonathan@gmail.com>
2022-05-02 21:18:38 +0200

    Started on Agario Server

---
+++ assignments/AgarioServer/AgarioServer.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System.Net;
+using System.Net.Sockets;
+
+public class AgarioServer
+{
+    
+    static void Main()
+    {
+        var endpoint = new IPEndPoint(IPAddress.Loopback, 1313);
+
+        var tcpListener = new TcpListener(endpoint);
+        tcpListener.Start();
+
+        ConnectToServer();
+
+
+
+    }
+
+    public static async Task ConnectToServer()
+    {
+        while (true)
+        {
+            
+        }
+    }
+}
+
