ad93cfa3a2bab02d7a9e2d4f6c2337599484a09d
JonathanJonsson <jonsson.h.jonathan@gmail.com>
2022-05-04 20:14:58 +0200

    ClientSide: Made level-loading on start, set up MainClient
    Main client now stores the players TcpClient and player name into next scene

--- assignments/Agario/Assets/MainClient.cs
+++ assignments/Agario/Assets/MainClient.cs
@@ -1,12 +1,28 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using UnityEngine;
 
 public class MainClient : MonoBehaviour
 {
+    [SerializeField]
+    private TcpClient client;
+    [SerializeField]
+    private string playerName;
+
     private void Start()
     {
         DontDestroyOnLoad(this);
     }
+
+    public void Init(TcpClient client, string playerName)
+    {
+        this.client = client;
+        this.playerName = playerName;
+
+        Debug.Log(client.Client.LocalEndPoint);
+        Debug.Log(playerName);
+    }
+    
 }
--- assignments/Agario/Assets/StartGame.cs
+++ assignments/Agario/Assets/StartGame.cs
@@ -6,16 +6,25 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartGame : MonoBehaviour
 {
+
+    [SerializeField]
+    private MainClient MainClient;
+    [SerializeField]
+    private TMP_InputField nameField;
     public void Connect()
     {
         var tcpClient = new TcpClient();
         tcpClient.Connect(IPAddress.Loopback, 1313);
         Debug.Log($"Connected to: {tcpClient.Client.LocalEndPoint}");
+        var playerName = nameField.text;
+        MainClient.Init(tcpClient, playerName);
         SceneManager.LoadScene("AgarioMain");
 
     }
